dac01937c3e979e6b5bd75ca29896c9ab0fc519a
unknown <yourajsharma2003@gmail.com>
2023-03-12 10:31:40 +0530

    Salting and Hashing Passwords with bcrypt to advance the security of user Database.

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-require("dotenv").config(); 
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+// const md5 = require("md5"); (for Hashing)
 // const  encrypt = require("mongoose-encryption");  (used for encryption of sensetive details in DB.)
 
 const app = express();
@@ -41,30 +44,37 @@ app.get("/register", function (req, res) {
 
 app.post("/register", function (req, res) {
 
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-    newUser.save().then(() => {
-        res.render("secrets");
-    })
-        .catch(err => {
-            // res.status(400).send("Unable to save post to database.");
-            console.log(err + "Unable to save post to database.");
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
         });
+        newUser.save().then(() => {
+            res.render("secrets");
+        })
+            .catch(err => {
+                // res.status(400).send("Unable to save post to database.");
+                console.log(err + "Unable to save post to database.");
+            });
+    })
 });
 
 app.post("/login", async (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
+
     try {
-        const foundUser = await User.findOne({ email: username});
-        if(foundUser){
-            if(foundUser.password == password) {
-                res.render("secrets");
-            } else {
-                res.send("Incorrect Password");
-            }
+        const foundUser = await User.findOne({ email: username });
+        if (foundUser) {
+            bcrypt.compare(password, foundUser.password, function (err, result) {
+                if (result === true) {
+                    res.render("secrets");
+                }
+                else {
+                    res.send("Incorrect Password");
+                }
+            });
+
         } else { res.send("User not found"); }
     }
     catch (err) { console.log(err); }
