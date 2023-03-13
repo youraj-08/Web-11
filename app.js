48ab78d2efbc38dd928a50e9132a4b66ecdb0c17
unknown <yourajsharma2003@gmail.com>
2023-03-13 21:53:42 +0530

    Prevent multiple Sign Ups from same email.

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const findOrCreate = require("mongoose-findorcreate");
 
+
 const app = express();
 
 app.use(express.static("public"));
@@ -42,12 +43,12 @@ const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(function(user, done){
+passport.serializeUser(function (user, done) {
     done(null, user.id);
 });
 
-passport.deserializeUser(function(id, done){
-    User.findById(id, function(err, user){
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
         done(err, user);
     });
 });
@@ -71,15 +72,15 @@ app.get("/", function (req, res) {
     res.render("home");
 });
 
-app.get("/auth/google", 
-passport.authenticate("google",{ scope : ["profile"] })
+app.get("/auth/google",
+    passport.authenticate("google", { scope: ["profile"] })
 );
 
 app.get("/auth/google/secrets",
-passport.authenticate("google", {failureRedirect: "/login"}),
-function(req,res){
-    res.redirect("/secrets");
-});
+    passport.authenticate("google", { failureRedirect: "/login" }),
+    function (req, res) {
+        res.redirect("/secrets");
+    });
 
 app.get("/login", function (req, res) {
     res.render("login");
@@ -90,35 +91,35 @@ app.get("/register", function (req, res) {
 });
 
 app.get("/secrets", function (req, res) {
-    User.find({"secret": {$ne: null}}, function(err, foundUsers){
-        if(err){
+    User.find({ "secret": { $ne: null } }, function (err, foundUsers) {
+        if (err) {
             console.log(err);
         } else {
-            if(foundUsers) {
-                res.render("secrets", {userWithSecrets: foundUsers});
+            if (foundUsers) {
+                res.render("secrets", { userWithSecrets: foundUsers });
             }
         }
     });
 });
 
-app.get("/submit", function(req, res){
-    if(req.isAuthenticated()){
+app.get("/submit", function (req, res) {
+    if (req.isAuthenticated()) {
         res.render("submit");
     } else {
         res.redirect("/login");
     }
 });
 
-app.post("/submit", function(req, res){
+app.post("/submit", function (req, res) {
     const submittedSecret = req.body.secret;
 
-    User.findById(req.user.id, function(err, foundUser){
-        if(err){
+    User.findById(req.user.id, function (err, foundUser) {
+        if (err) {
             console.log(err);
         } else {
-            if(foundUser) {
+            if (foundUser) {
                 foundUser.secret = submittedSecret;
-                foundUser.save(function(){
+                foundUser.save(function () {
                     res.redirect("/secrets");
                 })
             }
@@ -137,17 +138,24 @@ app.get("/logout", function (req, res) {
 
 app.post("/register", function (req, res) {
 
-    User.register({ username: req.body.username }, req.body.password, function (err, user) {
-        if (err) {
+    User.findOne({ username: req.body.username }, function (existingUser, err) {
+        if (existingUser === null) {
+            User.register({ username: req.body.username }, req.body.password, function (err, user) {
+                if (err) {
+                    console.log(err);
+                    res.redirect("/register");
+                } else {
+                    passport.authenticate("local")(req, res, function () {
+                        res.redirect("/secrets");
+                    });
+                }
+            })
+        } if (err) {
             console.log(err);
-            res.redirect("/register");
-        } else {
-            passport.authenticate("local")(req, res, function () {
-                res.redirect("/secrets");
-            });
         }
-    })
+    });
 });
+
 
 app.post("/login", async (req, res) => {
     const user = new User({
